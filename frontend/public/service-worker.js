d5a11bbbf3e50bf20fda67f4e283b7c679b852bd
revgrow7-commits <revgrow7@gmail.com>
2025-12-10 20:09:10 +0000

    auto-commit for 80b1adf8-4052-4b47-b104-9d9d75f6025f

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = 'industria-visual-v1';
+const CACHE_NAME = 'industria-visual-v2-updated';
 const urlsToCache = [
   '/',
   '/index.html',
