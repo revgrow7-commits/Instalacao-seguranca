ea2870d1141d85fcde717a6691ba316bd076edc0
revgrow7-commits <revgrow7@gmail.com>
2025-12-17 15:41:47 +0000

    auto-commit for 052d5c61-2479-4f71-b2d9-a1630615aea5

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, HTTPException, Depends, status, UploadFile, File, Form
+from fastapi import FastAPI, APIRouter, HTTPException, Depends, status, UploadFile, File, Form, Query
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.responses import FileResponse, StreamingResponse
 from dotenv import load_dotenv
